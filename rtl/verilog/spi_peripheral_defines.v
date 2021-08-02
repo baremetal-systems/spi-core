8db55e8d6588cadbfb52fe92d79eb4b2b9c61e7c
Digital Runecaster <info@baremetal-systems.de>
2021-08-02 21:13:52 +0200

    worked on defines for SPI peripheral

--- rtl/verilog/spi_peripheral_defines.v
+++ rtl/verilog/spi_peripheral_defines.v
@@ -1,3 +1,29 @@
 `include "timescale.v"
 `default_nettype none
 
+`define SPI_BUS_WIDTH_8
+//`define SPI_BUS_WIDTH_16
+//`define SPI_BUS_WIDTH_32
+
+`ifdef SPI_BUS_WIDTH_8
+    `define SPI_BUS_WIDTH	    8
+    `define SPI_COUNT_WIDTH	    3
+`endif
+
+`ifdef SPI_BUS_WIDTH_16
+    `define SPI_BUS_WIDTH	    16
+    `define SPI_COUNT_WIDTH	    4
+`endif
+
+`ifdef SPI_BUS_WIDTH_32
+    `define SPI_BUS_WIDTH	    32
+    `define SPI_COUNT_WIDTH	    5
+`endif
+
+`define SPI_REG_CNT		    8
+
+`define SPI_CPOL		    0
+`define SPI_CPHA		    0
+`define SPI_LSB_FRST		    0
+
+
